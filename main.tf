c16c2bd8e7991f45b9fc0e2c6aa2585806dcd002
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-08 12:18:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  tenant_id         = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+}
+
 resource "azurerm_resource_group" "anne_terraform_rg" {
     name = "anne_tdjenkinsterra"
     location = francencentral
