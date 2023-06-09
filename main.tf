6129620c41a9767d89347d652ce320c520788650
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-09 09:39:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  subscription_id   = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
   tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-#client_id       = "384ba4e4-fc1a-47f4-bf42-acec6fad1e82"
-#client_secret   = "lxk8Q~LfBuObGY4frVfc6B_e4Nqf6UMahS-BdaD-"
+  client_id       = "092a1cb7-ef14-4074-a70e-06816f49f212"
+  client_secret   = "93a12ba5-05d9-4b95-8f83-09e519fb8588"
 }
 
 resource "azurerm_resource_group" "anne_terraform_rg" {
