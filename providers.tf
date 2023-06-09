42e82b7caad05fdb11885d5703c6fd7a5b7c2f1a
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-09 10:42:11 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+  client_id       = "9000611a-9990-454d-a270-59d326b7577d"
+  client_secret   = "hDR8Q~QQE7obehzK5TAAUPawfvuURWsZi_BzaciT"
+}
