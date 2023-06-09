585da5721dcb0c717e8f8ba98284f1f4ff6e289e
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-09 10:00:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+  subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
   tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-  client_id       = "092a1cb7-ef14-4074-a70e-06816f49f212"
-  client_secret   = "93a12ba5-05d9-4b95-8f83-09e519fb8588"
+  client_id       = "9000611a-9990-454d-a270-59d326b7577d"
+  client_secret   = "hDR8Q~QQE7obehzK5TAAUPawfvuURWsZi_BzaciT"
 }
 
 resource "azurerm_resource_group" "anne_terraform_rg" {
