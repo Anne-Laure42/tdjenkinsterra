61fe6569704d72e5b7abeac72a356f487542b953
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-09 11:17:42 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,6 +13,6 @@ provider "azurerm" {
 
   subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
   tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-  client_id       = "9000611a-9990-454d-a270-59d326b7577d"
-  client_secret   = "hDR8Q~QQE7obehzK5TAAUPawfvuURWsZi_BzaciT"
+  client_id       = "03a82c66-e1aa-4a04-9f36-b0252a491750"
+  client_secret   = "qpY8Q~liOK-CE8mJDsQ3VtQlD-aQIBixs.QNVdfP"
 }
