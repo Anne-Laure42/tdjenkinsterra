86084d27198d84d14ac69c4ff4763445415936e9
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-06-09 11:11:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,113 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-
-  subscription_id   = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
-  tenant_id       = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
-  client_id       = "9000611a-9990-454d-a270-59d326b7577d"
-  client_secret   = "hDR8Q~QQE7obehzK5TAAUPawfvuURWsZi_BzaciT"
-}
-
 resource "azurerm_resource_group" "anne_terraform_rg" {
-    name = "anne_tdjenkinsterra"
-    location = "France Central"
+   name = "anne_tdjenkinsterra"
+   location = var.location
+}
+
+
+resource "azurerm_network_security_group" "allowedports" {
+   name = "allowedports"
+   resource_group_name = azurerm_resource_group.anne_terraform_rg.name
+   location = azurerm_resource_group.anne_terraform_rg.location
+
+   security_rule {
+       name = "http"
+       priority = 100
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "8080"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "https"
+       priority = 200
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "443"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+
+   security_rule {
+       name = "ssh"
+       priority = 300
+       direction = "Inbound"
+       access = "Allow"
+       protocol = "Tcp"
+       source_port_range = "*"
+       destination_port_range = "22"
+       source_address_prefix = "*"
+       destination_address_prefix = "*"
+   }
+}
+
+resource "azurerm_public_ip" "anne_terraform_public_ip" {
+   name = "anne_brief13_public_ip"
+   location = var.location
+   resource_group_name = azurerm_resource_group.anne_terraform_rg.name
+   allocation_method = "Dynamic"
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.anne_terraform_rg]
+}
+
+resource "azurerm_network_interface" "anne_terraform_ni" {
+   name = "anne_nginx-interface"
+   location = azurerm_resource_group.anne_terraform_rg.location
+   resource_group_name = azurerm_resource_group.anne_terraform_rg.name
+
+   ip_configuration {
+       name = "internal"
+       private_ip_address_allocation = "Dynamic"
+       subnet_id = azurerm_subnet.anne_terraform_subnet.id
+       public_ip_address_id = azurerm_public_ip.anne_terraform_public_ip.id
+   }
+
+   depends_on = [azurerm_resource_group.anne_terraform_rg]
+}
+
+resource "azurerm_linux_virtual_machine" "anne_terraform_vmnginx" {
+   size = var.instance_size
+   name = "anne_brief13_nginx"
+   resource_group_name = azurerm_resource_group.anne_terraform_rg.name
+   location = azurerm_resource_group.anne_terraform_rg.location
+   custom_data = base64encode(file("init-script.sh"))
+   network_interface_ids = [
+       azurerm_network_interface.anne_terraform_ni.id,
+   ]
+
+   source_image_reference {
+       publisher = "Canonical"
+       offer = "UbuntuServer"
+       sku = "18.04-LTS"
+       version = "latest"
+   }
+
+   computer_name = "nginx"
+   admin_username = "adminuser"
+   admin_password = "kingpin42330@"
+   disable_password_authentication = false
+
+   os_disk {
+       name = "nginxdisk01"
+       caching = "ReadWrite"
+       #create_option = "FromImage"
+       storage_account_type = "Standard_LRS"
+   }
+
+   tags = {
+       environment = var.environment
+       costcenter = "it"
+   }
+
+   depends_on = [azurerm_resource_group.anne_terraform_rg]
 }
